734c03e9ba4c81f35006d699864140f3ec8997b0
AnyPerson1 <arda45simsek@gmail.com>
2025-03-12 15:09:16 +0300

    settings scriptable object

---
+++ PogoProject/Assets/Scripts/Settings/GameSetting.cs
@@ -0,0 +1,51 @@
+using UnityEngine;
+
+public enum FPS
+{
+    Low = 30,
+    Mid = 60,
+    High = 144,
+    Unlimited = -1
+}
+public enum AntiAliasing
+{
+    None,
+    FXAA,
+    MSAA,
+    TAA
+}
+public enum Quality
+{
+    Low,
+    Medium,
+    High
+}
+
+[CreateAssetMenu(fileName = "GameSetting", menuName = "Scriptable Objects/GameSetting")]
+public class GameSetting : ScriptableObject
+{
+    [Header("Ekran Ayarlarý")]
+    [SerializeField] public int rWidth = 1920;
+    [SerializeField] public int rHeight = 1080;
+    [SerializeField] public FPS fps = FPS.Mid; 
+    [SerializeField] public bool vsync = true; 
+    [SerializeField] 
+    [Header("Grafik Ayarlarý")]
+    public bool shadows = true;                  
+    [SerializeField] public bool postprocessing = true;          
+    [SerializeField] public AntiAliasing antialiasing = AntiAliasing.FXAA; 
+    [SerializeField] public Quality antialiasingQuality = Quality.Medium;
+
+    [Header("Ses Ayarlarý")]
+    [Range(0, 100)] public int volume = 100;
+
+    public void ApplySettings()
+    {
+        Screen.SetResolution(rWidth, rHeight, FullScreenMode.FullScreenWindow);
+        QualitySettings.vSyncCount = vsync ? 1 : 0;
+        Application.targetFrameRate = (int)fps;
+        QualitySettings.antiAliasing = antialiasing == AntiAliasing.MSAA ? 4 : 0;
+        Debug.Log("Ayarlar uygulandý!");
+    }
+}
+
---
+++ PogoProject/Assets/Scripts/Settings/SettingsManager.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class SettingsManager : MonoBehaviour
+{
+    [SerializeField] private GameSetting gameSetting;
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            gameSetting.ApplySettings();
+        }
+    }
+}
+
