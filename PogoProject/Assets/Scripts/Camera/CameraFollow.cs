ef63186397af9ba8fe0d10e76a29f648abb32d60
Aytekin <143596814+AytekinJ@users.noreply.github.com>
2025-03-20 03:19:16 +0300

    tutorial uptaded, star and armor added...

--- PogoProject/Assets/Scripts/Camera/CameraFollow.cs
+++ PogoProject/Assets/Scripts/Camera/CameraFollow.cs
@@ -7,9 +7,11 @@ public class CameraFollow : MonoBehaviour
 	public float smooth = 0.125f;
 
 	Vector3 velocity = Vector3.zero;
-
+    //-3.5 normal, -0.65 up
     private void Update() {
-        Vector3 movePosition = new Vector3(target.position.x, -4) + offset;
+        Vector3 movePosition = new Vector3(target.position.x, target.position.y) + offset;
 		transform.position = Vector3.SmoothDamp(transform.position, movePosition, ref velocity, smooth);
+        
+        
     }
 }
