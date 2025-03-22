0a8795438d6207709c45836952c54c346475b562
EgeKaban <139078002+EgeKaban@users.noreply.github.com>
2025-03-22 11:51:43 +0300

    Update GeneralCamera.cs

--- PogoProject/Assets/Scripts/Camera/GeneralCamera.cs
+++ PogoProject/Assets/Scripts/Camera/GeneralCamera.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 public class GeneralCamera : MonoBehaviour
 {
     public Vector3 offset = new Vector3(0, 0, -10);
-    public float smooth = 0.125f;
+    public float smoothValue = 15f;
     Vector3 velocity = Vector3.zero;
 
     public static Transform TransformToLock;
@@ -54,6 +54,6 @@ public class GeneralCamera : MonoBehaviour
     void Lerpcam()
     {
         Vector3 movePosition = new Vector3(TransformToLock.position.x, TransformToLock.position.y, offset.z);
-        transform.position = Vector3.SmoothDamp(transform.position, movePosition, ref velocity, smooth);
+        transform.position = Vector3.SmoothDamp(transform.position, movePosition, ref velocity, smoothValue * Time.deltatime);
     }
 }
