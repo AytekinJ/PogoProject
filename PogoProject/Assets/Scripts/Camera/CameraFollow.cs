a2c8783eeb5b3d8dffab4fd29765255202d5e370
EgeKaban <egekaban@gmail.com>
2025-03-22 18:57:53 +0300

    smooth fix

--- PogoProject/Assets/Scripts/Camera/CameraFollow.cs
+++ PogoProject/Assets/Scripts/Camera/CameraFollow.cs
@@ -4,13 +4,13 @@ public class CameraFollow : MonoBehaviour
 {
     public Transform target;
 	public Vector3 offset = new Vector3(0, 0, -10);
-	public float smoothValue = 2.5f;
+	public float smoothValue = 0.2f;
 
 	Vector3 velocity = Vector3.zero;
     //-3.5 normal, -0.65 up
     private void Update() {
         Vector3 movePosition = new Vector3(target.position.x, target.position.y) + offset;
-		transform.position = Vector3.Lerp(transform.position, movePosition, smoothValue * Time.deltaTime);
+		transform.position = Vector3.SmoothDamp(transform.position, movePosition, ref velocity, smoothValue);
         
         
     }
