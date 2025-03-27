73945f23fd5fd430d762080b024c704cd24c8cff
EgeKaban <egekaban@gmail.com>
2025-03-28 02:59:42 +0300

    Basic SFX

--- PogoProject/Assets/Scripts/Player/AttackScript.cs
+++ PogoProject/Assets/Scripts/Player/AttackScript.cs
@@ -34,6 +34,8 @@ public class AttackScript : MonoBehaviour
     [SerializeField] float CamShakeDuration = 0.1f;
     [SerializeField] float CamShakeMagnitude = 0.05f;
 
+    [SerializeField] GameObject SwordSwaySFX;
+
     void Start()
     {
         playerController = GetComponent<Controller>();
@@ -49,6 +51,13 @@ public class AttackScript : MonoBehaviour
         SetLastAttackPos();
     }
 
+    void PlaySFX()
+    {
+        var sfx = Instantiate(SwordSwaySFX, transform.position, Quaternion.identity, gameObject.transform);
+        sfx.GetComponent<AudioSource>().pitch = UnityEngine.Random.Range(0.9f, 1.1f);
+        Destroy(sfx, 3f);
+    }
+
     void AppendAttack()
     {
         if (Input.GetKeyDown(AttackKey) && Time.time >= attacktime)
@@ -59,6 +68,8 @@ public class AttackScript : MonoBehaviour
             {
                 return;
             }
+
+            PlaySFX();
 
             if (attackDirection != Vector2.zero)
             {
