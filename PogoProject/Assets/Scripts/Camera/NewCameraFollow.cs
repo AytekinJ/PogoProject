bad4b5aab40176ff5e5d9eaabc91341d669f123b
EgeKaban <egekaban@gmail.com>
2025-04-22 21:22:35 +0300

    fixed camera x1000000

--- PogoProject/Assets/Scripts/Camera/NewCameraFollow.cs
+++ PogoProject/Assets/Scripts/Camera/NewCameraFollow.cs
@@ -8,131 +8,46 @@ public class NewCameraFollow : MonoBehaviour
     public Vector3 offset = new Vector3(0, 0, -10);
     public float smoothValueY = 0.2f;
     public float smoothValueX = 0.2f;
-    float velocity = 0;
-    Vector3 vel = Vector3.zero;
+
+    Vector3 velX = Vector3.zero;
+    Vector3 velY = Vector3.zero;
 
     public float MinYValue = 0f;
     public float yMultiplier = 3f;
-
-    //public Controller controllerScript;
-
-    #region old
-    //public Transform UnGroundedTransform;
-    //public Transform GroundedTransform;
-    #endregion
-    #region old
-    //public Controller controllerScript;
-    //[SerializeField] float DownY = 0.34f;
-    //public LayerMask GroundMask;
-    #endregion
-    #region old
-    //public Transform movePosition;
-    #endregion
-    #region old
-    //private Vector3 defaultOffset;
-    //float inputYHoldTime;
-    #endregion
+    float SavedMultiplier;
 
     private void Start()
     {
-        Instance = this;
-        //controllerScript = GameObject.FindGameObjectWithTag("Player").GetComponent<Controller>();
-        #region old
-        //movePosition.position = new Vector3(target.position.x, target.position.y, offset.z);
-        //controllerScript = GameObject.FindGameObjectWithTag("Player").transform.GetComponent<Controller>();
-        //defaultOffset = offset;
-        //SetUnGrounded();
-        //SetGrounded();
-        #endregion
+        if (Instance != null && Instance != this)
+            Destroy(gameObject);
+        else
+            Instance = this;
+
+        SavedMultiplier = yMultiplier;
     }
 
-    private void Update()
+    private void LateUpdate()
     {
-        #region old
-        //SetGrounded();
-        //SetUnGrounded();
-        //SetMovePosition();
-        #endregion
-        #region old
-        // if(Mathf.Abs(target.position.y - gameObject.transform.position.y) > 2.5)
-        // {
-        //     SetCamFollowPublic(target.position);
-        // }
-
-        //if (controllerScript.inputX == 0 && controllerScript.inputY != 0 && controllerScript.CheckGrounded())
-        //{
-        //    inputYHoldTime += Time.deltaTime;
-
-        //    if (inputYHoldTime > 1)
-        //    {
-        //        offset = new Vector3(defaultOffset.x, defaultOffset.y + 1 * controllerScript.inputY, defaultOffset.z);
-        //    }
-        //}
-        //else
-        //{
-        //    offset = defaultOffset;
-        //    inputYHoldTime = 0;
-        //}
-        #endregion
         Lerp();
     }
 
     void Lerp()
     {
-        
-        Vector3 xLerp;
-        Vector3 yLerp;
+        float targetX = target.position.x;
+        float targetY = target.position.y;
 
         if (Controller.Instance.playerRb.linearVelocityY >= MinYValue)
         {
-            xLerp = Vector3.SmoothDamp(transform.position, target.position, ref vel, smoothValueX);
-            yLerp = Vector3.SmoothDamp(transform.position, target.position, ref vel, smoothValueY);
-            transform.position = new Vector3(xLerp.x, yLerp.y, 0) + offset;
+            yMultiplier = 1f;
         }
-        else if (Controller.Instance.playerRb.linearVelocityY < MinYValue)
+        else
         {
-            xLerp = Vector3.SmoothDamp(transform.position, target.position, ref vel, smoothValueX);
-            yLerp = Vector3.SmoothDamp(transform.position, target.position, ref vel, smoothValueY * yMultiplier);
-            transform.position = new Vector3(xLerp.x, yLerp.y, 0) + offset;
+            yMultiplier = SavedMultiplier;
         }
 
-        
+        float newX = Mathf.SmoothDamp(transform.position.x, targetX, ref velX.x, smoothValueX);
+        float newY = Mathf.SmoothDamp(transform.position.y, targetY, ref velY.y, smoothValueY / yMultiplier);
 
-        
+        transform.position = new Vector3(newX, newY, 0f) + offset;
     }
-
-    #region old
-    //void SetMovePosition()
-    //{
-    //    if (controllerScript.CheckGrounded())
-    //    {
-    //        movePosition = GroundedTransform.position;
-    //    }
-    //    else
-    //    {
-    //        movePosition = new Vector3(target.position.x, GroundedTransform.position.y, UnGroundedTransform.position.z);
-    //    }
-    //}
-
-    //void SetUnGrounded()
-    //{
-    //    if (!controllerScript.CheckGrounded())
-    //    {
-    //        RaycastHit2D ray;
-    //        ray = Physics2D.Raycast(target.transform.position, Vector2.down, Mathf.Infinity, GroundMask);
-    //        UnGroundedTransform.position = new Vector3(ray.point.x, ray.point.y + 1f, offset.z);
-    //    }
-    //}
-
-    //void SetGrounded()
-    //{
-    //    if (controllerScript.CheckGrounded())
-    //        GroundedTransform.position = target.position + offset;
-    //}
-
-    //public void SetCamFollowPublic(Vector3 targetTransorm)
-    //{
-    //    GroundedTransform.position = targetTransorm + offset;
-    //}
-    #endregion
 }
