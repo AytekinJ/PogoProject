92e088f99aea2c7efe945d03530468d0969e8ab3
EgeKaban <egekaban@gmail.com>
2025-04-29 01:09:31 +0300

    kafayı yedim

--- PogoProject/Assets/Scripts/UI/EndScreenButtons.cs
+++ PogoProject/Assets/Scripts/UI/EndScreenButtons.cs
@@ -3,6 +3,17 @@ using UnityEngine.SceneManagement;
 
 public class EndScreenButtons : MonoBehaviour
 {
+
+    public LevelsManager levelsManager;
+
+    //void Awake()
+    //{
+    //    if (LevelsManager.instance == null)
+    //    {
+    //        LevelsManager.instance = Resources.Load<LevelsManager>("LevelsManager");
+    //    }
+    //}
+
     public void RestartGame()
     {
         Time.timeScale = 1f; 
@@ -18,12 +29,24 @@ public class EndScreenButtons : MonoBehaviour
     }
     public void NextLevel()
     {
-        string currentLevel = LevelsManager.instance.currentLevel.ToString();
-        if(int.Parse(currentLevel) == 7)
+        Debug.Log(levelsManager);
+        Debug.Log(levelsManager.currentLevel);
+        Debug.Log(levelsManager.currentLevel.ToString());
+        string currentLevel = levelsManager.currentLevel.ToString();
+        if(int.Parse(currentLevel) == 8)
         {
-            return; // şuanlık 7 dan büyük level olmadığı için işlem iptal
+            return; // şuanlık 8 dan büyük level olmadığı için işlem iptal
         }
-        LevelsManager.instance.currentLevel++;
+        else if (int.Parse(currentLevel) == SceneManager.sceneCountInBuildSettings - 4) //SonPlayable Shanede miyiz? toplam 9 sahne var ve oynanabilecek 6 sahne var ve 0 dan başlıyor, o yüzden max sahneden 4 çıkardım
+        {
+            //eğer öyleyse credits i yükle
+            //LevelsManager.instance.currentLevel++;
+            SceneData.SceneToLoad = "Credits";
+            SceneData.LoadScene();
+            Time.timeScale = 1f;
+            return;
+        }
+        levelsManager.currentLevel++;
         SceneData.SceneToLoad = "Level "+(int.Parse(currentLevel)+2);
         SceneData.LoadScene();
         Time.timeScale=1f;
