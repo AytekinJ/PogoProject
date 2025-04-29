e0c3e4bdfc5d321e17efeb0f0ca5a140a3ece295
Aytekin <143596814+AytekinJ@users.noreply.github.com>
2025-04-29 23:41:05 +0300

    Update FPSCounter.cs

--- PogoProject/Assets/Scripts/UI/FPSCounter.cs
+++ PogoProject/Assets/Scripts/UI/FPSCounter.cs
@@ -2,49 +2,49 @@ using UnityEngine;
 
 public class FPSCounter : MonoBehaviour
 {
-    public bool showFPS = false;
-    public float updateInterval = 0.5f; 
-    public int fontSize = 24;
-    public Color textColor = Color.white;
-    public Vector2 position = new Vector2(10, 10);
+    // public bool showFPS = false;
+    // public float updateInterval = 0.5f; 
+    // public int fontSize = 24;
+    // public Color textColor = Color.white;
+    // public Vector2 position = new Vector2(10, 10);
 
-    private float accum = 0; 
-    private int frames = 0;
-    private float timeLeft; 
-    private float fps = 0;
+    // private float accum = 0; 
+    // private int frames = 0;
+    // private float timeLeft; 
+    // private float fps = 0;
 
-    private GUIStyle style;
+    // private GUIStyle style;
 
-    private void Start()
-    {
-        timeLeft = updateInterval;
+    // private void Start()
+    // {
+    //     timeLeft = updateInterval;
 
-        style = new GUIStyle();
-        style.fontSize = fontSize;
-        style.normal.textColor = textColor;
-    }
+    //     style = new GUIStyle();
+    //     style.fontSize = fontSize;
+    //     style.normal.textColor = textColor;
+    // }
 
-    private void Update()
-    {
-        timeLeft -= Time.deltaTime;
-        accum += Time.timeScale / Time.deltaTime;
-        frames++;
+    // private void Update()
+    // {
+    //     timeLeft -= Time.deltaTime;
+    //     accum += Time.timeScale / Time.deltaTime;
+    //     frames++;
 
    
-        if (timeLeft <= 0.0)
-        {
-            fps = accum / frames;
-            timeLeft = updateInterval;
-            accum = 0.0f;
-            frames = 0;
-        }
-    }
+    //     if (timeLeft <= 0.0)
+    //     {
+    //         fps = accum / frames;
+    //         timeLeft = updateInterval;
+    //         accum = 0.0f;
+    //         frames = 0;
+    //     }
+    // }
 
-    private void OnGUI()
-    {
-        if (showFPS)
-        {
-            GUI.Label(new Rect(position.x, position.y, 200, 50), "FPS: " + fps.ToString("F2"), style);
-        }
-    }
+    // private void OnGUI()
+    // {
+    //     if (showFPS)
+    //     {
+    //         GUI.Label(new Rect(position.x, position.y, 200, 50), "FPS: " + fps.ToString("F2"), style);
+    //     }
+    // }
 }
